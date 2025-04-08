75a927fb12d15ff735345cd3b036e30da6911da5
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-04-08 20:34:20 +0530

    Finally done

--- app.js
+++ app.js
@@ -23,31 +23,58 @@ document.addEventListener("keypress", function(){
             btn.classList.remove("flash");
         }, 250);
      }
-     function userFlash(btn){
-        btn.classList.add("userFlash");
-        setTimeout(function(){
-         btn.classList.remove("userFlash");
-        }, 250);
-     }
+    //  function userFlash(btn){
+    //     btn.classList.add("userFlash");
+    //     setTimeout(function(){
+    //      btn.classList.remove("userFlash");
+    //     }, 250);
+    //  }
 
     function levelUp(){
+        userSeq = [];
         level++;
         h2.innerText = `level ${level}`;
 
         let randIndx = Math.floor(Math.random()*4);
         let randColor = btns[randIndx];
         let randBtn = document.querySelector(`.${randColor}`);
-
+        gameSeq.push(randColor);
         gameFlash(randBtn);
     }
+
+    function checkAns(idx){
+        if(userSeq[idx] === gameSeq[idx]){
+            if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1200);
+            }
+        }else{
+            h2.innerHTML = `Game Over your score is <b>${level}<b> <br> <b>Press any key to start again<b>`;
+            document.querySelector("body").style.backgroundColor = "red";
+            setTimeout(function(){
+                document.querySelector("body").style.backgroundColor = "white"}
+                , 200);
+            reset();
+        }
+    };
     function btnPress(){
         console.log(this);
         let btn = this;
-        userFlash(btn);
+        gameFlash(btn);
+
+        userColor = btn.getAttribute("id");
+        userSeq.push(userColor);
+        checkAns(userSeq.length-1);
     }
 
     let allBtns = document.querySelectorAll(".box");
     for(btn of allBtns){
         btn.addEventListener("click", btnPress)
+    }
+
+   function reset(){
+        gameSeq = [];
+        userSeq = [];
+        level = 0;
+        started = false;
     }
 });
